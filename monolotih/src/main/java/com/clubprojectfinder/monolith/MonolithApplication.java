d7dca4139649ac4d258a53c9b0db85287350baf1
Joshua Jones <joshuanjones18@gmail.com>
2024-07-10 22:31:45 -0700

    spring init

---
+++ monolotih/src/main/java/com/clubprojectfinder/monolith/MonolithApplication.java
@@ -0,0 +1,15 @@
+package com.clubprojectfinder.monolith;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
+
+@SpringBootApplication
+@EnableMongoRepositories
+public class MonolithApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MonolithApplication.class, args);
+	}
+
+}
